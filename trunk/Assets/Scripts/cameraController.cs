be90511a0c18418c708ddb3e886bc19a53f6b9ac
trond.glomnes@gmail.com <trond.glomnes@gmail.com@cd4c3ed3-b5dc-0168-cfdf-6aa57e2a1e0f>
2014-03-17 13:33:21 +0000

    -added flappy bird

---
+++ trunk/Assets/Scripts/cameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraController : MonoBehaviour {
+
+	Transform bird;
+
+	// Use this for initialization
+	void Start () {
+		bird = GameObject.Find("Bird").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(bird != null){
+			transform.position = new Vector3(bird.position.x,0,-10);
+		}
+	}
+}
