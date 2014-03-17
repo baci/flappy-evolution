a895af5966df81e5e2bd73c1f41977af6168e407
trond.glomnes@gmail.com <trond.glomnes@gmail.com@cd4c3ed3-b5dc-0168-cfdf-6aa57e2a1e0f>
2014-03-17 13:33:21 +0000

    -added flappy bird

---
+++ Assets/Scripts/birdController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class birdController : MonoBehaviour {
+
+	public float forwardSpeed;
+
+	public float flapForce;
+
+	bool dead;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Flap();
+		}
+
+		if(gameObject.transform.position.y < -5){
+			destroyMe();
+		}
+
+		transform.right = rigidbody2D.velocity;
+
+		if(!dead){
+			rigidbody2D.velocity = new Vector2(forwardSpeed,rigidbody2D.velocity.y);
+		}
+	}
+
+	public void Flap(){
+		if(!dead){
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x,flapForce);
+		}
+	}
+
+	void destroyMe(){
+		Destroy(gameObject);
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		//print ("HELOP");
+		if(col.gameObject.tag == "Pipe"){
+			dead = true;
+			collider2D.isTrigger = true;
+		}
+	}
+}
---
+++ Assets/Scripts/gameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameController : MonoBehaviour {
+
+	public float gravity;
+
+	// Use this for initialization
+	void Start () {
+		Physics2D.gravity = new Vector2(0,-gravity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
