ebb1701256c9c69a5ed3c3c8d39505ff58b64a4c
trond.glomnes@gmail.com <trond.glomnes@gmail.com@cd4c3ed3-b5dc-0168-cfdf-6aa57e2a1e0f>
2014-03-31 12:00:02 +0000

    -optimizing

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -6,7 +6,7 @@ public class cameraController : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		transform.position += Vector3.right*gameController.instance.forwardSpeed/60;
+		transform.position += Vector3.right*gameController.instance.forwardSpeed;
 	}
 
 }
