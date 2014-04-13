96582c6b6433170ca93c9db51629f2819e6674ba
vincependrell <vincependrell@cd4c3ed3-b5dc-0168-cfdf-6aa57e2a1e0f>
2014-04-13 17:54:06 +0000

    eventz

---
+++ Assets/NEAT/Experiments/Flappolution/BirdsEvaluation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using SharpNeat.Core;
+using SharpNeat.Phenomes;
+using System.Collections;
+
+public class BirdsEvaluation : IPhenomeEvaluator<IBlackBox> {
+
+	private ulong _evalCount;
+	private bool _stopConditionSatisfied;
+	
+	public ulong EvaluationCount { get{return _evalCount;} }
+	public bool StopConditionSatisfied { get{return _stopConditionSatisfied;} }
+
+	public FitnessInfo Evaluate(IBlackBox box){
+
+		ISignalArray inputSignal = box.InputSignalArray;
+		ISignalArray outputSignal = box.OutputSignalArray;
+
+
+		return FitnessInfo.Zero;
+	}
+
+	public void Reset(){
+		
+	}
+}
