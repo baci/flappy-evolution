96eca6fd32ddbdce338c0c851c476cc16150c3e3
trond.glomnes@gmail.com <trond.glomnes@gmail.com@cd4c3ed3-b5dc-0168-cfdf-6aa57e2a1e0f>
2014-04-16 13:54:20 +0000

    -braindead birds gets -1 in distance

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -6,6 +6,7 @@ public class cameraController : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
+		if(gameController.instance.isSimulating)
 		transform.position += Vector3.right*gameController.instance.forwardSpeed;
 	}
 
